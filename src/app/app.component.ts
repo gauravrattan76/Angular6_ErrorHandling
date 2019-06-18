c1b6f9973ef1353071e4d8f7fbeebd33e0f4f3d4
gaurav rattan <gauravrattan76@gmail.com>
2019-06-18 07:24:36 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import {messageService} from './messageService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers:[messageService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+  constructor(private _messageService :messageService){}
+  employeeList:any;
+  errorMessage:any;
+
+  ngOnInit(){
+       this._messageService.getEmployees().subscribe((data)=>
+       this.employeeList = data,
+       (error) => this.errorMessage = error)
+  }
 }
---
+++ src/app/messageService.ts
@@ -0,0 +1,28 @@
+import {HttpClient,HttpErrorResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import { Observable,throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable()
+export class messageService{
+
+    constructor(private _http :HttpClient){
+
+    }
+
+    getEmployees():Observable<any>{
+        return this._http.get<any>("http://localhost:3000/employees2")
+        .pipe(catchError(this.handleError))
+    }
+
+    handleError(errorResponse:HttpErrorResponse){
+        if(errorResponse.error instanceof ErrorEvent)
+        {
+            console.log("client side error" + errorResponse.error.message);
+        }
+        else{
+            console.log("server side error" + errorResponse);
+        }
+        return throwError("There is issue with the service please trya fter sometime");
+    }
+}
